cae0a2fa5541d6ef165b617898d3e82ee9869e27
Pablo Zamora <pablo.zamora@icloud.com>
2023-10-02 02:55:11 -0300

    1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,19 +5,26 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROYECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING,
-  appId: import.meta.env.VITE_APP_ID,
-};
+ const firebaseConfig = {
+   apiKey: "AIzaSyA1LjenrelOFQJbh2wkwo_2m7r08q-NbLg",
+   authDomain: "ecomercetech-5000c.firebaseapp.com",
+   projectId: "ecomercetech-5000c",
+   storageBucket: "ecomercetech-5000c.appspot.com",
+   messagingSenderId: "697127159390",
+   appId: "1:697127159390:web:1befb21da08cbfbec616cf",
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 
-
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROYECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
 
